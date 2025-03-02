9bb1d321a982708af032800182b7a45236cd6bae
hariprasadalli <hariprasadalli4@gmail.com>
2025-03-02 14:07:48 +0530

    sixth push entire

--- src/main/java/com/ninja/base/TestBase.java
+++ src/main/java/com/ninja/base/TestBase.java
@@ -18,42 +18,36 @@ public class TestBase {
 	public static Properties prop;
 	public static WebDriver driver;
 	public static WebDriver wait;
-	
-	
 
-    
-    
 	public TestBase() throws Exception {
 		try {
-			
+
 			prop = new Properties();
 			FileInputStream ip = new FileInputStream(
 					"D:\\SeleniumQspiders\\com.nopCommerce.qa\\src\\main\\java\\com\\demo\\config\\config.properties");
 			prop.load(ip);
-			
-			
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void initialization() {
-//		ChromeOptions options = new ChromeOptions();
-//        driver = new ChromeDriver(options);
-		
-		System.setProperty("webdriver.chrome.driver", "D:\\GLAMS\\chromedriver-win64\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().window().maximize();
-        driver.get(prop.getProperty("url"));
+		ChromeOptions options = new ChromeOptions();
+		driver = new ChromeDriver(options);
+
+//		System.setProperty("webdriver.chrome.driver", "D:\\GLAMS\\chromedriver-win64\\chromedriver.exe");
+//		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		driver.get(prop.getProperty("url"));
 	}
-	
+
 	public static void closeDriver() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
-	
+		if (driver != null) {
+			driver.quit();
+		}
+	}
 
 }
